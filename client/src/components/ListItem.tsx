1fd2987fb0f28e06d98b81bc9051306800385908
Murilo Pavanello <murilopava@gmail.com>
2026-04-26 22:23:29 -0300

    feat: adicionar HTTP requests nos produtos

--- client/src/components/ListItem.tsx
+++ client/src/components/ListItem.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Product } from "../types";
 
 interface ListItemProps {
@@ -14,33 +14,93 @@ const ListItem = ({
   shareId,
   setError,
 }: ListItemProps) => {
-  const removerItemDaLista = () => {};
+  const removeItemList = async () => {
+    try {
+      await fetch(
+        `http://localhost:3333/lists/${shareId}/products/${product.id}`,
+        {
+          method: "DELETE",
+        },
+      );
+      setProducts((prev) => prev.filter((p) => p.id !== product.id));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const updateQuant = async (newQuant: number) => {
+    try {
+      await fetch(
+        `http://localhost:3333/lists/${shareId}/products/${product.id}`,
+        {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ quant: newQuant }),
+        },
+      );
+
+      setProducts((prev) =>
+        prev.map((p) => (p.id === product.id ? { ...p, quant: newQuant } : p)),
+      );
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const updateItemState = async (newState: boolean) => {
+    try {
+      await fetch(
+        `http://localhost:3333/lists/${shareId}/products/${product.id}`,
+        {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ isPurchased: newState }),
+        },
+      );
+
+      setProducts((prev) =>
+        prev.map((p) =>
+          p.id === product.id ? { ...p, isPurchased: newState } : p,
+        ),
+      );
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <>
       <li className="align-items-center flex w-full gap-2">
         <label htmlFor="quant" className="flex w-full min-w-0 items-center">
           <p
-            className={`max-w-65 px-2 text-xl wrap-break-word ${itemComprado ? "text-gray-500 line-through decoration-4" : ""}`}
+            className={`max-w-65 px-2 text-xl wrap-break-word ${product.isPurchased ? "text-gray-500 line-through decoration-4" : ""}`}
           >
-            {listItem}
+            {product.name}
           </p>
         </label>
 
         <div className="flex items-center gap-2">
           <button
-            className={`h-6 w-6 rounded-full transition-colors ${itemComprado ? "bg-green-500 hover:bg-green-600" : "bg-red-500 hover:bg-red-600"} cursor-pointer`}
-            onClick={() => setItemComprado((c) => (c === false ? true : false))}
+            className={`h-6 w-6 rounded-full transition-colors ${product.isPurchased ? "bg-green-500 hover:bg-green-600" : "bg-red-500 hover:bg-red-600"} cursor-pointer`}
+            onClick={
+              product.isPurchased
+                ? async () => await updateItemState(false)
+                : async () => await updateItemState(true)
+            }
           >
-            {listItem ? "✔️" : "❌"}
+            {product.isPurchased ? "✔️" : "❌"}
           </button>
 
           <button
-            className={`rounded-md ${itemComprado ? "cursor-pointer bg-red-600 transition hover:bg-red-500" : "cursor-not-allowed bg-gray-500"} px-2 text-white`}
-            disabled={!listItem}
+            className={`rounded-md ${product.isPurchased ? "cursor-pointer bg-red-600 transition hover:bg-red-500" : "cursor-not-allowed bg-gray-500"} px-2 text-white`}
+            disabled={!product.isPurchased}
             onClick={() => {
-              setErro("");
-              removerItemDaLista();
+              setError("");
+              removeItemList();
             }}
           >
             Remover
@@ -49,8 +109,8 @@ const ListItem = ({
           <input
             className="align-center w-10 rounded-sm border border-gray-300 p-0 text-center"
             type="number"
-            value={quant}
-            onChange={(e) => setQuant(Number(e.target.value))}
+            value={product.quant}
+            onChange={(e) => updateQuant(Number(e.target.value))}
             max={20}
             min={0}
             name="quant"
@@ -60,13 +120,13 @@ const ListItem = ({
 
           <button
             className="cursor-pointer rounded-md bg-blue-600 px-2 font-bold text-white transition hover:bg-blue-500"
-            onClick={() => setQuant((q) => (q < 20 ? q + 1 : q))}
+            onClick={() => updateQuant(product.quant + 1)}
           >
             +
           </button>
           <button
             className="cursor-pointer rounded-md bg-blue-600 px-2 font-bold text-white transition hover:bg-blue-500"
-            onClick={() => setQuant((q) => (q > 0 ? q - 1 : 0))}
+            onClick={() => updateQuant(product.quant - 1)}
           >
             -
           </button>
