20ca0bf90d4ab9f5f2fe676cbfb9d11079d71dde
Murilo Pavanello <murilopava@gmail.com>
2026-05-12 00:11:56 -0300

    feat: adicionar biblioteca toast

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router";
 import Home from "./pages/Home";
 import Items from "./pages/Items.js";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
     <BrowserRouter>
+      <Toaster position="top-center" reverseOrder={false}></Toaster>
       <Routes>
         <Route path="/" element={<Navigate to="/lists" replace />}></Route>
         <Route path="/lists" element={<Home />}></Route>
